0c8ac5558209d5b8db0dcf316c8315afbe409a8c
Tom Fisher <thomasdenisfisher@gmail.com>
2016-09-21 17:32:04 +0100

    Small change

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,14 @@ function success(pos) {
   var crd = pos.coords;
   var response = JSON.parse(httpGet("http://api.openweathermap.org/data/2.5/weather?lat=" + crd.latitude + "&lon=" + crd.longitude +"&appid=999fa0575bb1b40bb6b2c958138a24aa&units=metric"));
   document.getElementById("basic-weather-holder").innerHTML = response.weather["0"].main;
-  document.getElementById("value-holder").innerHTML = parseInt(response.main.temp, 10) ;
+  document.getElementById("value-holder").innerHTML = parseInt(response.main.temp, 10);
+  document.getElementById("unit-holder").innerHTML = "C";
+  var days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+  var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  var currentdate = new Date();
+  document.getElementById("day-holder").innerHTML = days[currentdate.getDay()];
+  document.getElementById("date-holder").innerHTML = currentdate.getDate();
+  document.getElementById("month-holder").innerHTML = months[currentdate.getMonth()];
 };
 
 function httpGet(theUrl)
@@ -22,14 +29,5 @@ function httpGet(theUrl)
 function error(err) {
   console.warn('ERROR(' + err.code + '): ' + err.message);
 };
-
-var days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
-var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-
-var currentdate = new Date();
-
-document.getElementById("day-holder").innerHTML = days[currentdate.getDay()];
-document.getElementById("date-holder").innerHTML = currentdate.getDate();
-document.getElementById("month-holder").innerHTML = months[currentdate.getMonth()];
 
 navigator.geolocation.getCurrentPosition(success, error, options);
